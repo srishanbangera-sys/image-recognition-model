7874e9747a7025fe319c4d5e7b4fdcf3b9fe1de8
Srishan Bangera <srishanbangera@gmail.com>
2026-09-15 09:53:12 +0530

    Adds initial Flask application and user registration
    Sets up basic Flask routes for user registration and image recognition pages.
    
    Implements the `/register` endpoint to handle form submissions, collecting personal details and multiple image uploads. Stores user data and images (converted to binary format) in the database for later use.

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request
+from bson.binary import Binary
+import numpy as np
+import os
+
+from utils.database import collection
+from utils.preprocess import preprocess_image
+from config import IMAGE_SIZE
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("register.html")
+
+
+@app.route("/register", methods = ["POST"])
+def register():
+
+    name = request.form["name"]
+    age = request.form["age"]
+    job = request.form["job"]
+    phone = request.form["phone"]
+    address = request.form["address"]
+
+    files = request.files.getlist("photos")
+
+    label = collection.count_documents({})
+    image_binary = [Binary(file.read()) for file in files]
+
+    person = {
+        "name" : name, 
+        "age" : age,
+        "job" : job,
+        "phhone" : phone,
+        "address" : address,
+        "label" : label,
+        "images" : image_binary
+    }
+
+
+    collection.insert_one(person)
+
+    return "Person Registered Sucessfully"
+
+@app.route("/recognize")
+def recognize_page():
+    return render_template("recognize.html")
+
+
+
+
+
+
